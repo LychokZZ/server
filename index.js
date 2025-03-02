be817a5c2c8faae72b2f6bf42c08d33804e3bfbb
IVAN LUCHYK <128646720+LychokZZ@users.noreply.github.com>
2025-03-03 00:43:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const PORT = process.env.PORT || 5001
 const mongoose = require('mongoose');
 const router = require('./authRouter');
-
+const os = require('os');
 
 const app =express()
 
@@ -19,6 +19,23 @@ app.use('/auth' ,router)
 const start = async () => {
     try {
         await mongoose.connect(`mongodb+srv://admin:admin@clus.qrigh.mongodb.net/?retryWrites=true&w=majority&appName=Clus`)
+
+        const networkInterfaces = os.networkInterfaces();
+        let ipAddress = '';
+        
+        for (const iface in networkInterfaces) {
+            for (const alias of networkInterfaces[iface]) {
+                if (alias.family === 'IPv4' && !alias.internal) {
+                    ipAddress = alias.address;
+                    break;
+                }
+            }
+            if (ipAddress) break; // Найден внешний IP-адрес
+        }
+
+        console.log(`Server is running at http://${ipAddress}:${PORT}`);
+
+        
         app.listen(PORT, '0.0.0.0', () => console.log(`Server started on port ${PORT}`));
     }catch(e){
         console.log(e)
