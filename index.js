296162760fdcebd4ea2afc4ab425ca6410baa20d
IVAN LUCHYK <128646720+LychokZZ@users.noreply.github.com>
2025-03-03 02:41:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const mongoose = require('mongoose');
 const router = require('./authRouter');
 const os = require('os');
 
-const app =express()
+const allowedOrigins = ["http://localhost:3000", "https://lychokzz.github.io"];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
-const allowedOrigins = ["http://localhost:3000", "https://lychokzz.github.io/Luchkovski/"];
-app.use(cors({
-    origin: allowedOrigins
-    credentials: true,
-    
-}
-));
 app.use('/auth' ,router)
 
 const start = async () => {
