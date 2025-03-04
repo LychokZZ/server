b67db06b6adb8e18079431e78e3865b28bbd7f42
IVAN LUCHYK <128646720+LychokZZ@users.noreply.github.com>
2025-03-04 16:40:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const router = require('./authRouter');
 
 
